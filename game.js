a7ef3d41aa23a2d08f2e4ea9b242733a63a621b3
mehdi boumecid <codingmehdi1@gmail.com>
2024-09-03 22:44:38 +0100

    i switched between files

--- game.js
+++ game.js
@@ -2,67 +2,92 @@
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound() {
+let init = false;
 
-    let human;
-    let computer;
 
-    function getComputerChoice(){
-        let choice = Math.random();
-        if(choice<=0.3){
-            return "r";
-        }
-        else if(choice>=0.6){
-            return "p";
-        }
-        return "s";
+function getComputerChoice(){
+    let choice = Math.random();
+    if(choice<=0.3){
+        return "r";
     }
-    
-    function getHumanChoice(){
-        /*let choice = prompt("choose one of the options:rock,paper or scissors:").toLowerCase();
-        return choice;*/
-        let buttons = document.querySelector('.buttons');
-        let button;
-        buttons.addEventListener("click",(e)=>
-        {
-            button = e.target;
-            //console.log(button.value);
-            //console.log(typeof(button.value));
-            human =  button.value;
-            computer = getComputerChoice()
-        });
-        //return button;
+    else if(choice>=0.6){
+        return "p";
     }
-    while(human==undefined){
-        getHumanChoice();
-    }
-    //getHumanChoice();
+    return "s";
+}
+
+function evaluate(human,computer){
 
     if(human==computer){
-        console.log("Tie game");
+        
     }
     else if(human=="r"&&computer=="s" || human=="p"&&computer=="r" || human=="s"&&computer=="p" ){
-        console.log("You win!");
+
         humanScore++;
     }
     else{
-        console.log("You lose");
         computerScore++;
     }
 
-    let message = "You:"+humanScore+"--Computer:"+computerScore;
-    console.log(message);
+}
+
+
+
+function displayScore(){
+
+    let score1 = document.querySelector('.humanScore');
+    score1.textContent = humanScore;
+
+
+    let score2 = document.querySelector('.computerScore');
+    score2.textContent = computerScore;
+
+
+}
+
+function checkScore(){ 
+    let message = document.querySelector(".winner")
+    let text = 'is the winner !'
+
+
+    if(humanScore==5 || computerScore == 5){
+        if(humanScore==5){
+            message.textContent = 'you human '+text;
+        }
+        else{
+            message.textContent = "computer "+text;
+        }
+        humanScore = 0;
+        computerScore = 0;
+        init = true;
+        
+    }
+    else{
+    message.textContent = 'first to reach five points wins!';}
+    
     
 }
 
-/*function playGame(){
-    for(let i =0;i<50000;i++){
-        playRound();
-    }
-}
 
-playGame();*/
-/*while(humanScore!=5 || computerScore!=5){
-    playRound();
-}*/
-playRound();
+let d = document.querySelector('.buttons');
+d.addEventListener('click',(e)=>{
+
+    if(!init){
+        let val = e.target;
+        let human = val.value;
+        let computer = getComputerChoice();
+        
+        
+        evaluate(human,computer);
+
+        displayScore();
+        checkScore();
+        //init = false;
+    }
+    else{
+        displayScore();
+        init = false;
+    }
+
+}
+)
